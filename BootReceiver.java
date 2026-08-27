b1edbe039eac2799c12d5ec7eeae98749c3c59e2
hz-zwix <bunyaminakar211@gmail.com>
2026-08-27 22:38:53 +0300

    Create BootReceiver.java

---
+++ BootReceiver.java
@@ -0,0 +1,17 @@
+package com.camerarat;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) ||
+            intent.getAction().equals("android.intent.action.QUICKBOOT_POWERON")) {
+            
+            Intent serviceIntent = new Intent(context, CameraService.class);
+            context.startForegroundService(serviceIntent);
+        }
+    }
+}
